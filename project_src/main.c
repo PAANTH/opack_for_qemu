6ce6ddb8f48bb1cd26268bc945430dc9d4e4f5b5
akashkin <akashkin@ancud.ru>
2020-06-02 14:04:32 +0300

    simple jump

--- project_src/main.c
+++ project_src/main.c
@@ -2,6 +2,9 @@
 #include "inttypes.h"
 #include "stm32f1xx_hal.h"
 
+
+typedef void (*pFun)(void);
+
 /*!
   \brief config_sysclk
         Функция настраивает системное тактирование на 168MHz.
@@ -51,6 +54,7 @@ void config_hw(void)
 
 
   __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   //__HAL_RCC_USART1_CLK_ENABLE();
   __HAL_RCC_TIM4_CLK_ENABLE();
 
@@ -63,7 +67,12 @@ void config_hw(void)
 
 //LED4
 
-
+//button
+  gpio_init_struct.Pin       = GPIO_PIN_13;
+  gpio_init_struct.Mode      = GPIO_MODE_INPUT;
+  gpio_init_struct.Speed     = GPIO_SPEED_FREQ_HIGH;
+  gpio_init_struct.Pull     = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOC, &gpio_init_struct);
 
 
 // //USART1
@@ -91,27 +100,69 @@ void config_hw(void)
   HAL_TIM_Base_Init(&htim4);
 
 }
+
+
+
+
+/**
+  * @brief  Sets the vector table location and Offset.
+  * @param  NVIC_VectTab: specifies if the vector table is in RAM or FLASH memory.
+  *   This parameter can be one of the following values:
+  *     @arg NVIC_VectTab_RAM
+  *     @arg NVIC_VectTab_FLASH
+  * @param  Offset: Vector Table base offset field. This value must be a multiple
+  *         of 0x200.
+  * @retval None
+  */
+static void NVIC_SetVectorTable(uint32_t NVIC_VectTab, uint32_t Offset)
+{
+  SCB->VTOR = NVIC_VectTab | (Offset & (uint32_t)0x1FFFFF80);
+}
+/**
+  \brief jump to main program
+  \param[in] addr - main program address
+  \return none
+*/
+// void __jump(uint32_t addr){
+
+//   uint32_t jump_address = *(__IO uint32_t*)(addr+4);
+//   pFun jump_to_app;
+
+//   uint32_t NVIC_VectTab_FLASH = 0x08000000U;
+//   //uint32_t NVIC_VectTab_SRAM = 0x02000000U;
+//   uint32_t app_offset = 0x20000U; //half flash mem
+//   jump_to_app = (pFun)jump_address;
+//   __set_PRIMASK(1);
+//   NVIC_SetVectorTable(NVIC_VectTab_FLASH, app_offset);
+//   __set_PRIMASK(0);
+//   __set_MSP(*(__IO uint32_t*)addr);
+//   jump_to_app();
+
+// }
+
+void jump_to_sram(void)
+{
+  memcpy(0x20000000, 0x80020000, updater_size);
+  SCB->VTOR = 0x20000000;
+  NVIC_SystemReset();
+}
+
 uint8_t test_buf[1024];;
+
 int main (void)
 {
-  //uint32_t lat;
-  //uint32_t retval = 0;
-  // uint32_t start = 0;
-  // uint32_t curr = 0;
+  GPIO_PinState button_pressed = GPIO_PIN_SET;
   SysTick_Config(72000);
   config_sysclk();
   config_hw();
 
   HAL_TIM_Base_Start(&htim4);
-  //start = HAL_GetTick();
   while(1) {
-    // curr = HAL_GetTick();
-    // retval = curr - start;
-    // if (retval >= 1000) {
-    //   USART1->DR = 'A';
-    //   start = curr;
-    // }
-    test_buf[4] = 7;
+    button_pressed = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13);
+    if (button_pressed == GPIO_PIN_RESET) {
+      __jump(0x08020000U);
+    }
+
   }
 }
 

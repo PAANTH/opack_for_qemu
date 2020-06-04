8dcadcbfceb3f5633be928ff2e03c8194270b9fd
akashkin <akashkin@ancud.ru>
2020-06-04 15:15:53 +0300

    jump to sram is functional

--- project_src/main.c
+++ project_src/main.c
@@ -4,8 +4,6 @@
 #include "stm32f1xx_hal.h"
 
 
-typedef void (*pFun)(void);
-
 /*!
   \brief config_sysclk
         Функция настраивает системное тактирование на 168MHz.
@@ -16,7 +14,6 @@ uint32_t config_sysclk(void)
   RCC_ClkInitTypeDef rcc_clk_init_str;
   RCC_OscInitTypeDef rcc_osc_init_str;
   __HAL_RCC_PWR_CLK_ENABLE();
-  //__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   rcc_osc_init_str.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   rcc_osc_init_str.Prediv1Source  = RCC_PREDIV1_SOURCE_PLL2;
@@ -50,23 +47,15 @@ void config_hw(void)
 {
   GPIO_InitTypeDef  gpio_init_struct;
 
-  //UART_HandleTypeDef husart1;
-
-
-
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
-  //__HAL_RCC_USART1_CLK_ENABLE();
   __HAL_RCC_TIM4_CLK_ENABLE();
 
 //LED0
-  gpio_init_struct.Pin       = GPIO_PIN_0;
+  gpio_init_struct.Pin       = GPIO_PIN_0 | GPIO_PIN_15;
   gpio_init_struct.Mode      = GPIO_MODE_OUTPUT_PP ;
   gpio_init_struct.Speed     = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(GPIOB, &gpio_init_struct);
-
-
-//LED4
 
 //button
   gpio_init_struct.Pin       = GPIO_PIN_13;
@@ -74,22 +63,6 @@ void config_hw(void)
   gpio_init_struct.Speed     = GPIO_SPEED_FREQ_HIGH;
   gpio_init_struct.Pull     = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOC, &gpio_init_struct);
-
-
-// //USART1
-//   gpio_init_struct.Pin       = GPIO_PIN_7 | GPIO_PIN_6;
-//   gpio_init_struct.Mode      = GPIO_MODE_AF_PP;
-//   gpio_init_struct.Speed     = GPIO_SPEED_FREQ_HIGH;
-//   HAL_GPIO_Init(GPIOB, &gpio_init_struct);
-
-//   husart1.Instance = USART1;
-//   husart1.Init.BaudRate = 115200;
-//   husart1.Init.WordLength = UART_WORDLENGTH_8B;
-//   husart1.Init.StopBits = UART_STOPBITS_1;
-//   husart1.Init.Parity = UART_PARITY_NONE;
-//   husart1.Init.Mode = UART_MODE_TX_RX;
-//   HAL_UART_Init(&husart1);
-
 
   htim4.Instance = TIM4;
   htim4.Init.Prescaler = 7200;
@@ -103,54 +76,16 @@ void config_hw(void)
 }
 
 
-
-
-/**
-  * @brief  Sets the vector table location and Offset.
-  * @param  NVIC_VectTab: specifies if the vector table is in RAM or FLASH memory.
-  *   This parameter can be one of the following values:
-  *     @arg NVIC_VectTab_RAM
-  *     @arg NVIC_VectTab_FLASH
-  * @param  Offset: Vector Table base offset field. This value must be a multiple
-  *         of 0x200.
-  * @retval None
-  */
-static void NVIC_SetVectorTable(uint32_t NVIC_VectTab, uint32_t Offset)
+void jump_to_sram(void)
 {
-  SCB->VTOR = NVIC_VectTab | (Offset & (uint32_t)0x1FFFFF80);
-}
-/**
-  \brief jump to main program
-  \param[in] addr - main program address
-  \return none
-*/
-void __jump(uint32_t addr){
-
-  uint32_t jump_address = *(__IO uint32_t*)(addr+4);
-  pFun jump_to_app;
-
-  uint32_t NVIC_VectTab_FLASH = 0x08000000U;
-  //uint32_t NVIC_VectTab_SRAM = 0x02000000U;
-  uint32_t app_offset = 0x20000U; //half flash mem
-  jump_to_app = (pFun)jump_address;
-  __set_PRIMASK(1);
-  NVIC_SetVectorTable(NVIC_VectTab_FLASH, app_offset);
-  __set_PRIMASK(0);
-  __set_MSP(*(__IO uint32_t*)addr);
-  jump_to_app();
-
+  __HAL_RCC_SYSCFG_CLK_ENABLE();
+  __disable_irq();
+  memcpy((uint8_t *)0x20000000, (uint8_t *)0x08020000, 7180);
+  void *reset_handler_addr = (void*)(*(uint32_t*)0x20000004);
+  void (*reset_handler)() = (void(*)())reset_handler_addr;
+  reset_handler();
 }
 
-// void jump_to_sram(void)
-// {
-//   memcpy((uint8_t *)0x20000000, (uint8_t *)0x80020000, 0x1da0);
-//   SCB->VTOR = 0x20000000;
-//   void *reset_handler_addr = (void*)(*(uint32_t*)0x20000004);
-//   void (*reset_handler)() = (void(*)())reset_handler_addr;
-//   reset_handler();
-// }
-
-uint8_t test_buf[1024];;
 
 int main (void)
 {
@@ -165,8 +100,8 @@ int main (void)
   while(1) {
     button_pressed = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13);
     if (button_pressed == GPIO_PIN_RESET) {
-      //jump_to_sram();
-      __jump(0x8020000);
+      HAL_TIM_Base_Stop(&htim4);
+      jump_to_sram();
     }
 
   }
@@ -175,7 +110,7 @@ int main (void)
 void TIM4_IRQHandler(void)
 {
   HAL_TIM_IRQHandler(&htim4);
-  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0 | GPIO_PIN_15);
   HAL_TIM_Base_Start(&htim4);
 }
 
@@ -212,3 +147,7 @@ Endless:
   goto Endless;
   __NOP();
 }
+
+
+
+
